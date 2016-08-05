f00efc77ea4c3d18ffb9fdc62e8f2d2b9ae68fdc
ghvik <vreddy@ischool.berkeley.edu>
2016-08-04 18:02:52 -0700

    Finished, fixed typo

--- HW03_string1.py
+++ HW03_string1.py
@@ -4,7 +4,7 @@
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
-# Google's Python Classl
+# Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
 # Basic string exercises
